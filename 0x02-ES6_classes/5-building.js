7913ea040ed3b6c93a000976337085b1091bd124
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-08-05 17:35:50 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
+    this.sqft = sqft;
     if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== "function") {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          "Class extending Building must override evacuationWarningMessage"
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
