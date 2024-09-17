0ffe685122f2fa1a0e5cd84d4243864bfccb482d
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-09-17 21:34:28 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf-8");
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-process.stdin.on("data", (input) => {
-  const name = input.toString();
-
-  console.log(`Your name is: ${name}`);
-
-  console.log("This important software is now closing");
-
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
